87522d4a79179c11b19c479aae28d287eb2452f8
Ana Clara <anaclrdev@gmail.com>
2025-11-25 15:53:41 -0300

    Comentários JS

--- js/script.js
+++ js/script.js
@@ -1,38 +1,58 @@
-window.onscroll = function() {showNavBar()};
-const links = document.querySelectorAll('a[href^="#"]');
 const menu = document.getElementById("main-menu");
 
-links.forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        const target = document.querySelector(link.getAttribute('href'));
-        const header = document.querySelector('header');
-        const headerOffset = header.offsetHeight;
-        const offsetTop = target.getBoundingClientRect().top + window.pageYOffset - headerOffset;
+// Mostra a barra de navegação ao rolar a tela
+window.onscroll = function() {
+    showNavBar()
+};
 
-        window.scrollTo({
-            top: offsetTop,
-            behavior: 'smooth'
+// Adiciona scroll suave assim que a página carrega
+window.onload = function () {
+    setLinkAnimation()
+};
+
+function setLinkAnimation() {
+    // Seleciona todos os links da página
+    links = document.querySelectorAll('a[href^="#"]');
+
+    // Percorre os links
+    links.forEach(link => {
+        link.addEventListener('click', e => {
+            // Retira o comporamento padrão
+            e.preventDefault();
+            // Armazena a referência do link
+            const target = document.querySelector(link.getAttribute('href'));
+            // Armazena a altura do cabeçalho
+            const headerOffset = document.querySelector('nav').offsetHeight;
+            // Posição do elemento alvo + posição atual da tela - altura do cabeçalho = destino do scroll
+            const offsetTop = target.getBoundingClientRect().top + window.pageYOffset - headerOffset;
+
+            // Scrolla suavemente até o destino
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth'
+            });
+
+            // Esconde o menu após o clique
+            hideMenu()
         });
-
-        hideMenu()
     });
-});
-
-function showNavBar() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
 }
 
+function showNavBar() {
+    // Ativa a navbar se a página scrollar mais de 10 pixels
+    const navbar = document.getElementById("navbar");
+    navbar.classList.toggle("active", window.pageYOffset > 10);
+}
+
+// ======== MOBILE ======== //
 function showMenu() {
-    menu.classList.toggle("collapsed");  
+    // Ativa o menu e troca o ícone
+    menu.classList.toggle("collapsed");  sdf
     document.getElementById("burger-icon").classList.toggle("active");
 };
 
 function hideMenu() {
+    // Desativa o menu e troca o ícone
     menu.classList.remove("collapsed"); 
     document.getElementById("burger-icon").classList.remove("active");    
 }
