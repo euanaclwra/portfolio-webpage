ce048c2eb47922e9e9b270d3d76c7d6e3ce4c991
Ana Clara <anaclrdev@gmail.com>
2025-11-26 10:08:28 -0300

    Requisição dos arquivos de idioma

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,2 +1,18 @@
 const translations = {};
-const url = "";
+
+// Carrega o arquivo JSON de um idioma
+async function loadLanguage(lang) {
+    try {        
+        const response = await fetch(`/i18n/${lang}.json`);
+        const data = await response.json();
+        translations[lang] = data;
+        return data;
+    } catch (error) {
+        console.error("Erro ao carregar tradução:", error)    
+    }
+}
+
+// Altera o conteúdo do HTML com base no idioma
+async function setLanguage(lang) {
+    const languageData = await loadLanguage(lang);
+}
